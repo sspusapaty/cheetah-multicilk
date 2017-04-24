93f8103ed792804bb0a927a0b00b6a247fa4ee8f
Matthew Kilgore <m.kilgore0@gmail.com>
2017-04-24 09:14:49 -0400

    Dumping cilkm sched code

--- runtime/sched.c
+++ runtime/sched.c
@@ -29,3 +29,57 @@ void worker_scheduler(__cilkrts_worker * w) {
   
   CILK_ASSERT(w == __cilkrts_get_tls_worker());
 }
+
+void Cilk_scheduler(struct __cilkrts_worker *const ws, Closure *t) {
+
+  /* 
+   * t contains 'the next thing to do'.  Initially, the
+   * scheduler on proc 0 executes the main closure.
+   */
+  int victim;
+  unsigned const int active_size = USE_PARAMETER_WS(active_size);
+
+
+  CILK_ASSERT(ws, ws->self >= 0);
+  CILK_ASSERT(ws, ws->l->magic == CILK_WS_MAGIC + ((unsigned int)ws->self));
+
+  rts_srand(ws, ws->self * 162347);
+
+  Cilk_enter_state(ws, STATE_TOTAL);
+
+  while (!USE_SHARED_WS(done)) {
+    if (!t) {
+      /* try to get work from our local queue */
+      deque_lock_self(ws);
+      t = deque_xtract_bottom(ws, ws->self);
+      deque_unlock_self(ws);
+    }
+
+    while (!t && !USE_SHARED_WS(done)) {
+      /* otherwise, steal */
+      Cilk_enter_state_unspecified(ws);
+            
+            
+
+      victim = rts_rand(ws) % active_size;
+      if( victim != ws->self ) {
+	t = Closure_steal(ws, victim);
+
+	if(!t && USE_PARAMETER_WS(options->yieldslice) &&
+	   !USE_SHARED_WS(done)) {
+	  Cilk_lower_priority(ws);
+	} 
+      }
+    }
+
+    if (!USE_SHARED_WS(done)) {
+      // if provably-good steals happened, it will contain
+      // the next closure to execute
+      t = do_what_it_says(ws, t);
+    }
+  }
+
+  Cilk_exit_state(ws, STATE_TOTAL);
+
+  return;
+}
