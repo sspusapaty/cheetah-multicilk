a462af34ac2f79826e322311d970c773825f4441
Matthew Kilgore <m.kilgore0@gmail.com>
2017-05-04 22:32:56 -0400

    Closure rework

--- runtime/closure.h
+++ runtime/closure.h
@@ -9,8 +9,9 @@ enum ClosureStatus { CLOSURE_RUNNING = 42, CLOSURE_SUSPENDED,
 enum AbortStatus { ABORT_ALL = 30 , ALMOST_NO_ABORT, NO_ABORT};
 
 // Includes
-#include "stack_frame.h"
 #include "cilk_mutex.h"
+#include "stack_frame.h"
+#include "fiber.h"
 #include "types.h"
 #include "common.h"
 /* 
@@ -19,54 +20,57 @@ enum AbortStatus { ABORT_ALL = 30 , ALMOST_NO_ABORT, NO_ABORT};
  * and locking.
  */
 struct Closure {
-    Cilk_mutex mutex;          /* mutual exclusion lock */
+  Cilk_mutex mutex;          /* mutual exclusion lock */
+
+  __cilkrts_stack_frame *frame;       /* rest of the closure */
+  cilk_fiber * fiber;
+  
   //WHEN_DEBUG_VERBOSE(int mutex_action;)
-  //WHEN_CILK_DEBUG(int mutex_owner;)
+  int mutex_owner;
 
-    int join_counter; /* number of spawned outstanding children */
-    int has_cilk_callee; /* has called outstanding cilk child */
-    enum ClosureStatus status;
+  int join_counter; /* number of spawned outstanding children */
 
-    /* the called cilk child closure --- used only for abort when program
-     * exit early by calling Cilk_exit */
-    Closure *callee;
+  enum ClosureStatus status;
 
-    Closure *call_parent; /* the "parent" closure that called */
-    Closure *spawn_parent; /* the "parent" closure that spawned */
+  Closure *call_parent; /* the "parent" closure that called */
+  Closure *spawn_parent; /* the "parent" closure that spawned */
 
-    __cilkrts_stack_frame *frame;       /* rest of the closure */
-    AddrType frame_rsp;
-    AddrType assumed_frame_rsp;
-    enum AbortStatus abort_status;
+  Closure *left_sib;  // left *spawned* sibling in the closure tree
+  Closure *right_sib; // right *spawned* sibling in the closur tree
+  // right most *spawned* child in the closure tree
+  Closure *right_most_child; 
 
-    Closure *left_sib;  // left *spawned* sibling in the closure tree
-    Closure *right_sib; // right *spawned* sibling in the closur tree
-    // right most *spawned* child in the closure tree
-    Closure *right_most_child; 
+  /*
+   * stuff related to ready deque.  These fields
+   * must be managed only by the queue manager in sched.c
+   *
+   * ANGE: for top of the ReadyDeque, prev_ready = NULL
+   *       for bottom of the ReadyDeque, next_ready = NULL
+   *       next_ready pointing downward, prev_ready pointing upward 
+   *
+   *       top
+   *  next | ^
+   *       | | prev
+   *       v |
+   *       ...
+   *  next | ^
+   *       | | prev
+   *       v |
+   *      bottom
+   */
+  Closure *next_ready;
+  Closure *prev_ready;
 
-    /*
-     * stuff related to ready deque.  These fields
-     * must be managed only by the queue manager in sched.c
-     *
-     * ANGE: for top of the ReadyDeque, prev_ready = NULL
-     *       for bottom of the ReadyDeque, next_ready = NULL
-     *       next_ready pointing downward, prev_ready pointing upward 
-     *
-     *       top
-     *  next | ^
-     *       | | prev
-     *       v |
-     *       ...
-     *  next | ^
-     *       | | prev
-     *       v |
-     *      bottom
-     */
-    Closure *next_ready;
-    Closure *prev_ready;
-
-    /* miscellanea */
-    unsigned int magic;
-    CILK_CACHE_LINE_PAD;
+  /* miscellanea */
+  unsigned int magic;
+  CILK_CACHE_LINE_PAD;
 };
+
+void Closure_assert_ownership(__cilkrts_worker *const ws, Closure *t);
+
+int Closure_trylock(__cilkrts_worker *const ws, Closure *t);
+
+void Closure_lock(__cilkrts_worker *const ws, Closure *t);
+
+void Closure_unlock(__cilkrts_worker *const ws, Closure *t);
 #endif
