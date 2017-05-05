2ae7bd833eae29624464840e5e5ff5a5bfa31811
Matthew Kilgore <m.kilgore0@gmail.com>
2017-05-04 20:40:48 -0400

    [Unchecked] scheduler overhaul

--- runtime/sched.c
+++ runtime/sched.c
@@ -7,79 +7,128 @@
 void longjmp_to_runtime(__cilkrts_worker * w) {
   __cilkrts_alert("Thread of worker %d: longjmp_to_runtime\n", w->self);
 
-  if (__builtin_expect(w->l->runtime_fiber != NULL, 1)) {
-    longjmp(w->l->runtime_fiber->ctx, 0);
-  } else {
-    w->l->runtime_fiber = cilk_fiber_allocate_from_heap();
-    cilk_fiber_set_owner(w->l->runtime_fiber, w);
+  longjmp(w->l->runtime_fiber->ctx, 1);
 
-    
+    /* 
     char * rsp = NULL;
     ASM_GET_SP(rsp);
     ASM_SET_SP(w->l->runtime_fiber->m_stack_base);
     worker_scheduler(w);
     ASM_SET_SP(rsp);
-    __cilkrts_alert("Thread of worker %d: exit longjmp_to_runtime\n", w->self);
-   }
+    */
+  __cilkrts_alert("Thread of worker %d: exit longjmp_to_runtime\n", w->self);
 }
   
 
-void worker_scheduler(__cilkrts_worker * w) {
-  __cilkrts_alert("Thread of worker %d: worker_scheduler\n", w->self);
-  
+
+static inline void reset_exception_pointer(__cilkrts_worker * ws, Closure *cl) {
+  ws->exc = ws->head;
+}
+
+Closure *setup_for_execution(__cilkrts_worker * ws, Closure *t) {
+  t->frame->worker = ws;
+  t->status = CLOSURE_RUNNING;
+
+  ws->head = (__cilkrts_stack_frame **) ws->l->shadow_stack+1;
+  ws->tail = (__cilkrts_stack_frame **) ws->l->shadow_stack+1;
+
+  /* push the first frame on the current_stack_frame */
+  ws->current_stack_frame = t->frame;	
+
+  reset_exception_pointer(ws, t);
+
+  return t;
+}
+
+Closure *do_what_it_says(__cilkrts_worker * ws, Closure *t) {
+
+  Closure *res = NULL;
+  __cilkrts_stack_frame *f;
+ 
+  Closure_lock(ws, t);
+
+  switch (t->status) {
+  case CLOSURE_READY:
+    /* just execute it */
+    setup_for_execution(ws, t);
+    f = t->frame;
+    
+    CILK_ASSERT(f);
+	  
+    Closure_unlock(ws, t);
+    
+    // MUST unlock the closure before locking the queue 
+    // (rule A in file PROTOCOLS)
+    deque_lock_self(ws);
+    deque_add_bottom(ws, t, ws->self);
+    deque_unlock_self(ws);
+    
+    /* now execute it */
+    __cilkrts_alert("Jump into user code (Worker %d).\n", ws->self);
+
+    cilk_fiber_suspend_self_and_resume_other(w->l->runtime_fiber, t->fiber_self);
+
+    __cilkrts_alert("Back from user code (Worker %d).\n", ws->self);
+
+    break; // ?
+  default:
+    __cilkrts_bug("BUG in do_what_it_says()\n");
+    break;
+  }
+
+  return res;
+}
+
+void worker_scheduler(__cilkrts_worker * w, Closure * t) {
   CILK_ASSERT(w == __cilkrts_get_tls_worker());
+  while (!w->g->done) {
+    if (!t) {
+      __cilkrts_alert("Thread of worker %d: worker_scheduler\n", w->self);
+      return;
+    }
+
+    do_what_it_says(w, t);
+  }
 }
 
-void Cilk_scheduler(struct __cilkrts_worker *const ws, Closure *t) {
+/*
+void Cilk_scheduler(__cilkrts_worker *const ws, Closure *t) {
+{
 
-  /* 
-   * t contains 'the next thing to do'.  Initially, the
-   * scheduler on proc 0 executes the main closure.
-   */
+
+   // t contains 'the next thing to do'.  Initially, the
+   // scheduler on proc 0 executes the main closure.
+
   int victim;
-  unsigned const int active_size = USE_PARAMETER_WS(active_size);
+  unsigned const int active_size = ws->g->active_size;
 
-
-  CILK_ASSERT(ws, ws->self >= 0);
-  CILK_ASSERT(ws, ws->l->magic == CILK_WS_MAGIC + ((unsigned int)ws->self));
+  CILK_ASSERT(ws->l->magic == CILK_WS_MAGIC + ((unsigned int)ws->self));
 
   rts_srand(ws, ws->self * 162347);
 
-  Cilk_enter_state(ws, STATE_TOTAL);
-
-  while (!USE_SHARED_WS(done)) {
+  while (!ws->g->done) {
     if (!t) {
-      /* try to get work from our local queue */
+      // try to get work from our local queue 
       deque_lock_self(ws);
       t = deque_xtract_bottom(ws, ws->self);
       deque_unlock_self(ws);
     }
 
-    while (!t && !USE_SHARED_WS(done)) {
-      /* otherwise, steal */
-      Cilk_enter_state_unspecified(ws);
-            
-            
+    while (!t && !ws->g->done) {
 
       victim = rts_rand(ws) % active_size;
       if( victim != ws->self ) {
 	t = Closure_steal(ws, victim);
-
-	if(!t && USE_PARAMETER_WS(options->yieldslice) &&
-	   !USE_SHARED_WS(done)) {
-	  Cilk_lower_priority(ws);
-	} 
       }
     }
 
-    if (!USE_SHARED_WS(done)) {
+    if (!ws->g->done) {
       // if provably-good steals happened, it will contain
       // the next closure to execute
       t = do_what_it_says(ws, t);
     }
   }
 
-  Cilk_exit_state(ws, STATE_TOTAL);
-
   return;
 }
+*/
--- runtime/sched.h
+++ runtime/sched.h
@@ -2,9 +2,14 @@
 #define _RSCHED_H
 
 #include "worker.h"
+#include "closure.h"
 
 void longjmp_to_runtime(__cilkrts_worker * w);
 
-void worker_scheduler(__cilkrts_worker * w);
+Closure *setup_for_execution(__cilkrts_worker * ws, Closure *t);
+
+Closure *do_what_it_says(__cilkrts_worker * ws, Closure *t);
+
+void worker_scheduler(__cilkrts_worker * w, Closure * t);
 
 #endif
